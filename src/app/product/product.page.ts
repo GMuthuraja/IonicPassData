5d959dcc6f4cab45a28a5d4d025c33bd6ca38ad0
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2019-12-12 17:37:44 +0530

    Changes made

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -7,7 +7,10 @@ import { GlobalDataService } from '../services/global-data.service';
   templateUrl: './product.page.html',
   styleUrls: ['./product.page.scss'],
 })
+
 export class ProductPage implements OnInit {
+
+  productID: any;
 
   constructor(private activatedRoute: ActivatedRoute, private global: GlobalDataService,
     private router: Router) { }
@@ -15,16 +18,19 @@ export class ProductPage implements OnInit {
   ngOnInit() {
     //Receiving from QueryParams
     this.activatedRoute.params.subscribe((param) => {
-      console.log("QueryParams: ", param['id']);
+      if(param)
+      this.productID = "QueryParams: " + param['id'];
     })
 
     //Receiving from Service
     var id = this.global.getID();
-    console.log("Service: ", id);
+    if(id)
+    this.productID = "Service: " + id;
 
 
     //Receiving from Extras
-    console.log("Extras: ", this.router.getCurrentNavigation().extras.state);
+    if(this.router.getCurrentNavigation().extras.state)
+    this.productID = "Extras: " + this.router.getCurrentNavigation().extras.state;
   }
 
 }
